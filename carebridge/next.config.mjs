a07b2afedbf391ebaefb4bf9d15d72f57ceba0b1
bazanty <bazantyloffan@gmail.com>
2025-02-20 12:19:50 +0300

    added herosection

--- carebridge/next.config.mjs
+++ carebridge/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['example.com', 'images.pexels.com', 'wwww.globalgiving.org'], // Add all the domains you need
+    },
+  };
+  
+  export default nextConfig;
+  
