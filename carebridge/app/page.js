e7b1f1ac8f4593b1e8d246a7ff04a45955d01014
Philip Mdicullar <phillip.emdokolo@student.moringaschool.com>
2025-02-20 19:06:13 +0300

    added medical page

--- carebridge/app/page.js
+++ carebridge/app/page.js
@@ -3,14 +3,16 @@ import Navbar from './components/Navbar'
 import Hero from './components/HeroSection'
 import HowItWorks from './components/Howitworks'
 import DisasterRecovery from './components/Disaster'
-
+import HealthMedical from './Health/HealthMedical'
+import HealthCharities from './Health/HealthCharities'
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
       <HowItWorks />
-      <DisasterRecovery />
+      <HealthMedical />
+      <HealthCharities />
     </div>
   )
 }
