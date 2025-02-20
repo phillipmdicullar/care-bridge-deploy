386835d7c29512be070b72df8458a33623c447ad
bazanty <bazantyloffan@gmail.com>
2025-02-20 13:24:14 +0300

    added disaster recovery that shows areas that have experienced disaster

--- carebridge/app/page.js
+++ carebridge/app/page.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/HeroSection'
 import HowItWorks from './components/Howitworks'
+import DisasterRecovery from './components/Disaster'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar />
       <Hero />
       <HowItWorks />
+      <DisasterRecovery />
     </div>
   )
 }
