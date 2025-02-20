44f6c8872aecef850129c8b818a158ef590313bc
bazanty <bazantyloffan@gmail.com>
2025-02-20 12:43:02 +0300

    added howitworks page thats gonna direct the donaters on how to donate

--- carebridge/app/page.js
+++ carebridge/app/page.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/HeroSection'
+import HowItWorks from './components/Howitworks'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <HowItWorks />
     </div>
   )
 }
